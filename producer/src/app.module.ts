c3f7522da0a443fd74f5f5fcbe75fb2d8147576c
JeongHyeongKim <maladroit1@likelion.org>
2022-08-08 20:05:10 +0900

    install packages and validation schema

--- producer/src/app.module.ts
+++ producer/src/app.module.ts
@@ -1,9 +1,25 @@
+import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        KAFKA_BROKER_HOST: Joi.string().required(),
+        KAFKA_BROKER_PORT: Joi.number().required(),
+        REDIS_USER: Joi.number().required(),
+        REDIS_PASSWORD: Joi.number().required(),
+        REDIS_PORT: Joi.number().required(),
+        REDIS_HOST: Joi.string().required(),
+        MODBUS_PORT: Joi.number().required(),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
